42aefbdeb156d9c81a4cc4c4724b48e6b8a7bb9a
DaniWinston25 <DaniWinston25@users.noreply.github.com>
2019-12-13 12:54:56 -0500

    updates

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,14 +2,12 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-axios 
-.get("https://api.github.com/users/DaniWinston25")
-.then((res)=>{
-console.log(res.data);
-})
-.catch((err)=>{
- console.log(err)
-});
+// axios 
+// .get("https://api.github.com/users/DaniWinston25")
+// .then((res)=>{
+// console.log(res.data);
+// })
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -20,8 +18,7 @@ const githubData = res.data
 /* Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
 */
-const mainCards = document.createElement('.cards')
-mainCards.appendChild(githubData);
+
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
           , manually find some other users' github handles, or use the list found 
@@ -34,6 +31,43 @@ mainCards.appendChild(githubData);
 
 const followersArray = [];
 
+
+function githubDataInfo(obj){
+
+  //creating elements//
+const card = document.createElement('div');
+const cardImg = document.createElement('img');
+const cardInfo = document.createElement('div');
+const name = document.createElement('h3');
+const username = document.createElement('p');
+const location = document.createElement('p');
+const profile = document.createElement('p');
+const profileLink =document.createElement('a');
+const followers = document.createElement('p');
+const following= document.createElement('p');
+const bio = document.createElement('p');
+
+///appending the children to their parents//
+  card.appendChild(cardImg);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(username);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  profile.appendChild(profileLink);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+
+  //creating classes//
+card.classList.add('card');
+cardInfo.classList.add('cardInfo');
+name.classList.add('name');
+username.classList.add('username');
+
+//creating text content//
+
+}
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
 
